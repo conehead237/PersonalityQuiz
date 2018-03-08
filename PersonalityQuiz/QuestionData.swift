a063e524d27e04df0682f3b0b61b8a2bc4cd9310
Ryan Kohn <rkohn1176@jh-mba158-13.d214.org>
2018-03-08 11:01:53 -0600

    1

---
+++ PersonalityQuiz/QuestionData.swift
@@ -0,0 +1,35 @@
+//
+//  QuestionData.swift
+//  PersonalityQuiz
+//
+//  Created by Ryan Kohn on 3/8/18.
+//  Copyright © 2018 johnherseyhighschool. All rights reserved.
+//
+
+import UIKit
+
+class QuestionData: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
